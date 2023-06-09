71694a44c7119a8e70c43af749edab910dfaa009
Yuchen <835033913@qq.com>
2023-06-09 22:19:38 +0800

    update tm1637

--- ESP32C3-TM1637/src/main.cpp
+++ ESP32C3-TM1637/src/main.cpp
@@ -1,3 +1,9 @@
+/*
+ * @Description:
+ * @Author: chenzedeng
+ * @Date: 2023-06-09 21:55:39
+ * @LastEditTime: 2023-06-09 22:11:08
+ */
 #include <Arduino.h>
 
 #include "tm1637.h"
@@ -6,15 +12,17 @@ unsigned int counter = 0;
 int point_s = 1;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println("Start...");
     tm_init();
+    tm_clear();
+    delay(100);
+    tm_set_point(point_s);
 }
 
 void loop() {
-    Serial.println("Loop~");
-    point_s = !point_s;
-    tm_set_point(point_s);
+    // point_s = !point_s;
+    // tm_set_point(point_s);
     tm_display(0, (counter / 1000) % 10);
     tm_display(1, (counter / 100) % 10);
     tm_display(2, (counter / 10) % 10);
